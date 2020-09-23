dcdfd6dc62490a242dae7d3e3e08d990228c3365
julio507 <julio_bruch@hotmail.com>
2020-09-22 21:44:44 -0300

    refeito funcionamento do desenho da tela

--- Makefile
+++ Makefile
@@ -2,6 +2,10 @@ PROJECT = x86-nibbles
 
 SOURCES += src/main.c
 
+ASSOURCES = boot/loader.S boot/isr.S
+
+LDSCRIPT = linker.ld
+
 LIBPATH = lib
 include $(LIBPATH)/lib.mk
 
@@ -9,7 +13,10 @@ CC = gcc
 AS = nasm
 LD = ld
 CP = cp
-DEL = rm
+MV = mv
+CD = cd
+DEL = rm -rf
+ZIP = zip
 QEMU = qemu-system-i386
 
 CFLAGS  += -nostdinc -nostdlib -nodefaultlibs -nostartfiles -static \
@@ -19,6 +26,12 @@ ASFLAGS = -f elf32 -g -F dwarf -O0
 LDFLAGS = -m elf_i386 -nostdlib
 
 OBJECTS = $(SOURCES:.c=.o)
+ASOBJECTS = $(ASSOURCES:.S=.o)
+
+DATE=`date +"%Y%m%d"`
+ZIPFILE=$(PROJECT)-$(DATE).zip
+ZIP_EXCLUDE=-x '*.git*' -x '*.sw*' -x '*.o' -x '*.zip'
+LOCATION=$(shell basename `pwd`)
 
 all: $(PROJECT).elf
 
@@ -26,8 +39,8 @@ $(PROJECT).iso: $(PROJECT).elf
 	$(CP) $< iso/boot/kernel.elf
 	grub-mkrescue -o $@ iso
 
-$(PROJECT).elf: boot/loader.o boot/isr.o $(OBJECTS)
-	$(LD) $(LDFLAGS) -T linker.ld -o $@ $^
+$(PROJECT).elf: $(ASOBJECTS) $(OBJECTS)
+	$(LD) $(LDFLAGS) -T $(LDSCRIPT) -o $@ $^
 
 %.o: %.S
 	$(AS) $(ASFLAGS) $< -o $@
@@ -44,4 +57,8 @@ run: $(PROJECT).iso
 debug: kernel.elf
 	$(QEMU) -soundhw pcspk -serial mon:stdio -s -S -kernel $< &
 	gdb -iex "set auto-load safe-path .gdbinit" $<
+
+zip:
+	$(DEL) *.zip
+	$(CD) .. && $(ZIP) -r $(ZIPFILE) $(LOCATION) $(ZIP_EXCLUDE) && $(MV) $(ZIPFILE) $(LOCATION)
 
--- src/main.c
+++ src/main.c
@@ -3,14 +3,22 @@
 #include <gfx/video.h>
 
 #define BACKGROUND_COLOR 0x00FFFF
-#define PLAYER_COLOR 0x000000
+#define PLAYER_COLOR 0xFFFFFF
 #define FRUIT_COLOR 0xFF0000
+#define LEFT 0x4b
+#define RIGHT 0x4d
+#define UP 0x48
+#define DOWN 0x50
 
 int playerX = 10;
 int playerY = 300;
 
 int fruitX = 500;
 int fruitY = 300;
+
+int speed = 5;
+
+char direction = 0;
 
 char pk;
 
@@ -29,6 +37,39 @@ void draw_fruit(int x, int y)
     draw_square(x, y, 10, 10, FRUIT_COLOR);
 }
 
+void clear(int x, int y)
+{
+    draw_square(x, y, 10, 10, BACKGROUND_COLOR);
+}
+
+void isr0()
+{
+    clear(playerX, playerY);
+
+    switch (direction)
+    {
+    case UP:
+        playerY -= speed;
+        break;
+
+    case DOWN:
+        playerY += speed;
+        break;
+
+    case LEFT:
+        playerX -= speed;
+        break;
+
+    case RIGHT:
+        playerX += speed;
+        break;
+    }
+
+    draw_player(playerX, playerY);
+
+    draw_fruit(fruitX, fruitY);
+}
+
 void isr1()
 {
     char k = inb(0x60);
@@ -37,27 +78,13 @@ void isr1()
     {
         pk = k;
 
-        switch (k)
+        if (k == LEFT || k == RIGHT || k == UP || k == DOWN)
         {
-        case 0x48:
-            draw_square(playerX, playerY - 10, 10, 10, BACKGROUND_COLOR);
-            draw_player(playerX, playerY);
-            break;
+            direction = k;
+        }
 
-        case 0x50:
-            draw_square(playerX, playerY++, 10, 10, BACKGROUND_COLOR);
-            draw_player(playerX, playerY);
-            break;
-
-        case 0x4b:
-            draw_square(playerX--, playerY, 10, 10, BACKGROUND_COLOR);
-            draw_player(playerX, playerY);
-            break;
-
-        case 0x4d:
-            draw_square(playerX++, playerY, 10, 10, BACKGROUND_COLOR);
-            draw_player(playerX, playerY);
-            break;
+        if (playerX == fruitX && playerY == fruitY)
+        {
         }
     }
 }
@@ -73,10 +100,6 @@ int main(unsigned long addr)
                 mbi->framebuffer_height);
 
     draw_board();
-
-    draw_player(playerX, playerY);
-
-    draw_fruit(fruitX, fruitY);
 
     while (1)
     {
